622d89451d6f8de66abcda2d1551451ddc8e2050
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-06-29 21:53:56 +0500

    Add lazy loading for secondary frontend routes

--- crud-frontend/src/App.tsx
+++ crud-frontend/src/App.tsx
@@ -1,21 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import CreateUser from "./CreateUser";
-import NotFound from "./NotFound";
-import UpdateUser from "./UpdateUser";
 import Users from "./Users";
 // import Home from './pages/Home'
+
+const CreateUser = lazy(() => import("./CreateUser"));
+const UpdateUser = lazy(() => import("./UpdateUser"));
+const NotFound = lazy(() => import("./NotFound"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/create" element={<CreateUser />} />
-        <Route path="/update/:id" element={<UpdateUser />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="app-shell d-flex justify-content-center align-items-center">
+            <div className="form-panel border bg-white rounded p-4 shadow-sm text-center">
+              <div className="lazy-loader text-muted">Loading page...</div>
+            </div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/update/:id" element={<UpdateUser />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
